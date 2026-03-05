d92b460738f260f1152d2d89bb43b2a80d560114
Aaron Stephen <aaronstephendonal2006@gmail.com>
2026-03-06 03:56:12 +0800

    changed the display of you are now logged in

--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['user'])) {
 
 <h2>Welcome, <?php echo htmlspecialchars($_SESSION['user']); ?>!</h2>
 
-<p>You are successfully logged in.</p>
+<p>You are now logged in. </p>
 <a href="profile.php">Go to Profile</a>
 <a href="logout.php">Logout</a>
 
